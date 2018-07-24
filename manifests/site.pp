6872346b6582bfe380cc535088a12ce9b712fe10
Besmir Zanaj [CLA] <besmirzanaj@gmail.com>
2018-07-24 04:21:38 -0400

    added node data

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,12 @@
 node default {
   class { 'base': }
-  class { 'cloudalbania': }
-}
-
-node 'vps1.cloudalbania.com' {
-  class { 'base': }
+  #class { 'cloudalbania': }
 }
 
 class base {
   class {motd:}
   class {ntp:}
+  class { 'cloudalbania': }
   class { 'resolv_conf':
     nameservers => ['1.1.1.1', '8.8.8.8.'],
     searchpath  => [ 'cloudalbania.com'],
