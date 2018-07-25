43b6675fa70c82ad44b2467aca854bbac504b6ad
Besmir Zanaj [CLA] <besmirzanaj@gmail.com>
2018-07-24 23:55:09 -0400

    further cleanup

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,7 @@
 node default {
-#  class { 'base': }
   class { cloudalbania::packages: }
-  class { 'resolv_conf':}
-}
+  class { resolv_conf: }
+  class { motd: }
+  class { ntp: }
 
-class base {
-  class {motd:}
-  class {ntp:}
-  class { 'resolv_conf':
-    nameservers => ['1.1.1.1', '8.8.8.8'],
-    searchpath  => [ 'cloudalbania.com'],
-  }
 }
-
